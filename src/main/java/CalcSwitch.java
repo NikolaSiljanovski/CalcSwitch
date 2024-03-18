61ab3b0b2ffe6fb4ed521bb6801d891dc66a9677
NikolaSiljanovski <siljanovski0@gmail.com>
2024-03-18 11:54:16 +0100

    Add Changes.java

---
+++ src/main/java/CalcSwitch.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class CalcSwitch {
+
+	public static void main(String[] args) {
+
+		
+		Scanner scan = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.println("Vnesi go prviot broj: ");
+		double a = input.nextDouble();
+		
+		System.out.println("Vnesi ja posakuvanata operacija: ");
+		String operacija = scan.nextLine();
+		
+		
+		System.out.println("Vnesi go vtoriot broj ");
+		double b = input.nextDouble();
+		
+		double c;
+		
+		switch(operacija) {
+		
+		
+		case "+" :
+			
+			c=a+b;
+			System.out.println(a + " " + operacija + " " + b  + " = "  +  c);
+			break;
+			
+		case "-" :
+			
+			c=a-b;
+			System.out.println(a + " " + operacija + " " + b  + " = "  +  c);
+			break;
+			
+		case "*" :
+			
+			c=a*b;
+			System.out.println(a + " " + operacija + " " + b  + " = "  +  c);
+			break;
+			
+		case "/" :
+			
+			c=a/b;
+			System.out.println(a + " " + operacija + " " + b  + " = "  +  c);
+			
+			break;
+			
+		
+			
+			
+			default:
+				System.out.println("Proverete sto imate napisano");
+				break;
+				
+		}
+		
+	
+		
+		
+	}
+
+}
